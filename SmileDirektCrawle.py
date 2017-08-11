e5c3d5d5d4796e87ed4ead4b037a95b9ea841247
behar21 <behar21@gmail.com>
2017-08-11 10:49:37 +0200

    Add files via upload

--- SmileDirektCrawle.py
+++ SmileDirektCrawle.py
@@ -9,6 +9,7 @@ import time
 from selenium.webdriver.common.keys import Keys
 import openpyxl
 from selenium.webdriver.support.ui import Select
+from tqdm import tqdm
 import signal
 import sys
 
@@ -25,200 +26,204 @@ sheets = wb.sheetnames
 ws = wb[sheets[0]]
 
 #-------------- Ends: Load Excel ------------------------
-successBar = progressbar.ProgressBar(maxval=31, \
-    widgets=[progressbar.Bar('=', 'Success bar: [', ']'), ' ', progressbar.Percentage()])
 
 
-successBar.start()
+pbar = tqdm(total=1)
 
-for i in range(2,31):
-	index = str(i)
-	successBar.update(i)
-	# ------------- Starts: Reading Excel -------------------
-	sdId = ws['A'+index].value
-	brand = ws['P'+index].value
-	model = ws['T'+index].value+" "+ws['U'+index].value
-	inverhrkersetzung = ws['V'+index].value
-	accessories = ws['R'+index].value
-	leasing = ws['H'+index].value
-	gender = ws['J'+index].value
-	use = ws['I'+index].value
-	licenceAge = ws['K'+index].value
-	nat = ws['L'+index].value
-	bd = '01.01.1976'
-	deductibleTeil = ws['N'+index].value
-	deductibleVoll = ws['O'+index].value
+try:
+	for i in range(2,3):
+		index = str(i)
+		
+		# ------------- Starts: Reading Excel -------------------
+		sdId = ws['A'+index].value
+		brand = ws['P'+index].value
+		model = ws['T'+index].value+" "+ws['U'+index].value
+		inverhrkersetzung = ws['V'+index].value
+		accessories = ws['R'+index].value
+		leasing = ws['H'+index].value
+		gender = ws['J'+index].value
+		use = ws['I'+index].value
+		licenceAge = ws['K'+index].value
+		nat = ws['L'+index].value
+		bd = '01.01.1976'
+		deductibleTeil = ws['N'+index].value
+		deductibleVoll = ws['O'+index].value
 	
-	licenceExperience = 2017-licenceAge
+		licenceExperience = 2017-licenceAge
 	
-	if licenceExperience == 4:
-		bd = '01.01.1994'
-	elif licenceExperience < 5:
-		bd = '01.01.1996'
+		if licenceExperience == 4:
+			bd = '01.01.1994'
+		elif licenceExperience < 5:
+			bd = '01.01.1996'
 		
 	
-	zipcode = ws['M2'].value
+		zipcode = ws['M2'].value
 
 	
-	#-------------- Ends: Reading Excel ---------------------
+		#-------------- Ends: Reading Excel ---------------------
 	
-	try:
-		driver = webdriver.Chrome()
-		driver.set_window_size(1280, 1060)
+		try:
+			driver = webdriver.Chrome()
+			driver.set_window_size(1280, 1060)
 	
-		driver.get("https://app.smile-direct.ch/car?lang=de&sac=WOA")
+			driver.get("https://app.smile-direct.ch/car?lang=de&sac=WOA")
 
 		
-		driver.find_element_by_xpath("//label[@for='markeundtyp_radio']").click()
+			driver.find_element_by_xpath("//label[@for='markeundtyp_radio']").click()
 
-		time.sleep(1)
+			time.sleep(1)
 
-		driver.find_element_by_xpath("//*[@id='brand']/option[text()='"+brand+"']").click()
-		time.sleep(1)
+			driver.find_element_by_xpath("//*[@id='brand']/option[text()='"+brand+"']").click()
+			time.sleep(1)
 	
-		driver.find_element_by_xpath("//*[@id='type']/option[text()='"+model+"']").click()
-		time.sleep(1)
+			driver.find_element_by_xpath("//*[@id='type']/option[text()='"+model+"']").click()
+			time.sleep(1)
 	
 	
 
-		driver.find_element_by_xpath("//*[@id='inverkehrssetzungsJahr']/option[text()='2017']").click()
-		driver.find_element_by_xpath("//*[@id='neuwertZubehoer']").send_keys(int(accessories))
-		time.sleep(1)
-		rowNumbers = len(driver.find_elements_by_xpath("//*[@id='modellist_tbody']/tr"))
-		driver.find_element_by_xpath("//*[@id='modellist_tbody']/tr["+str(rowNumbers)+"]/td[1]/label").click()
-		time.sleep(1)
-		if leasing == 'yes':
-			driver.find_element_by_xpath("//label[@for='finanzierung-leasing']").click()
-		time.sleep(1)
-		if gender == 'Male':
-			driver.find_element_by_xpath("//*[@id='smiForm']/div[3]/div/div/div/ul[2]/li[10]/div[2]/div/label[1]").click()
-		elif gender == 'Female':
-			driver.find_element_by_xpath("//*[@id='smiForm']/div[3]/div/div/div/ul[2]/li[10]/div[2]/div/label[2]").click()
-		time.sleep(1)
-		driver.find_element_by_xpath("//*[@id='fahrzeugLenkerGeburtsdatum']").send_keys(bd)
-		time.sleep(1)	
-		driver.find_element_by_xpath("//*[@id='nationLenker']/option[text()='"+nat+"']").click()
+			driver.find_element_by_xpath("//*[@id='inverkehrssetzungsJahr']/option[text()='2017']").click()
+			driver.find_element_by_xpath("//*[@id='neuwertZubehoer']").send_keys(int(accessories))
+			time.sleep(1)
+			rowNumbers = len(driver.find_elements_by_xpath("//*[@id='modellist_tbody']/tr"))
+			driver.find_element_by_xpath("//*[@id='modellist_tbody']/tr["+str(rowNumbers)+"]/td[1]/label").click()
+			time.sleep(1)
+			if leasing == 'yes':
+				driver.find_element_by_xpath("//label[@for='finanzierung-leasing']").click()
+			time.sleep(1)
+			if gender == 'Male':
+				driver.find_element_by_xpath("//*[@id='smiForm']/div[3]/div/div/div/ul[2]/li[10]/div[2]/div/label[1]").click()
+			elif gender == 'Female':
+				driver.find_element_by_xpath("//*[@id='smiForm']/div[3]/div/div/div/ul[2]/li[10]/div[2]/div/label[2]").click()
+			time.sleep(1)
+			driver.find_element_by_xpath("//*[@id='fahrzeugLenkerGeburtsdatum']").send_keys(bd)
+			time.sleep(1)	
+			driver.find_element_by_xpath("//*[@id='nationLenker']/option[text()='"+nat+"']").click()
 
-		if nat != 'Schweiz':
-			driver.find_element_by_xpath("//*[@id='bewilligungLenkerLabel']/option[text()='C']").click()
-		driver.find_element_by_xpath("//*[@id='fahrzeugLenker.postleitzahl']").send_keys(int(zipcode))
-		time.sleep(1)
-		driver.find_element_by_xpath("//*[@id='anzahlKinder']").send_keys(0)
-		time.sleep(1)
-		driver.find_element_by_xpath("//*[@id='datumFahrpruefungPkw']").send_keys(int(licenceAge))
+			if nat != 'Schweiz':
+				driver.find_element_by_xpath("//*[@id='bewilligungLenkerLabel']/option[text()='C']").click()
+			driver.find_element_by_xpath("//*[@id='fahrzeugLenker.postleitzahl']").send_keys(int(zipcode))
+			time.sleep(1)
+			driver.find_element_by_xpath("//*[@id='anzahlKinder']").send_keys(0)
+			time.sleep(1)
+			driver.find_element_by_xpath("//*[@id='datumFahrpruefungPkw']").send_keys(int(licenceAge))
 
-		if use == 'Private':
-			driver.find_element_by_xpath("//*[@id='car_private_label']").click()
-		elif use == 'Commute':	
-			driver.find_element_by_xpath("//*[@id='car_privateway_label']").click()
-		elif use == 'Business':
-			driver.find_element_by_xpath("//*[@id='car_privatebusiness_label']").click()
-		time.sleep(1)
-		driver.find_element_by_xpath("//*[@id='smiForm']/div[3]/div/div/div/div/li/button").click()
-		#wait.until(EC.element_located_to_be_selected((By.ID,'smiForm')))
-		#----------------------------------------------------------------------------------------------------
-		driver.find_element_by_xpath("//*[@id='smiForm']/div[2]/div/ul/li[1]/div[1]/div/label[2]").click()
-		driver.find_element_by_xpath("//*[@id='smiForm']/div[2]/div/ul/li[3]/div[1]/div/label[2]").click()
-		driver.find_element_by_xpath("//*[@id='smiForm']/div[2]/div/ul/li[5]/div[1]/div/label[2]").click()
-		driver.find_element_by_xpath("//*[@id='schaeden']/div[1]/div/label[2]").click()
-		driver.find_element_by_xpath("//*[@id='weiter']").click()
-		#-----------------------------------------------------------------------------------------------------
-		if leasing == 'no':
-			time.sleep(2)
-			driver.find_element_by_xpath("//*[@id='haftpflicht_content']/table/tbody/tr[2]/td[2]/div/div/div/div/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div/div/div/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='insassenunfall_acc']/li/div/div/div/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='gfverzicht_mod']/div/ul/li/div/div/div/div/label[1]").click()
-			time.sleep(5)
-			firstValue = driver.find_element_by_xpath("//*[@id='bruttopraemie']").text
+			if use == 'Private':
+				driver.find_element_by_xpath("//*[@id='car_private_label']").click()
+			elif use == 'Commute':	
+				driver.find_element_by_xpath("//*[@id='car_privateway_label']").click()
+			elif use == 'Business':
+				driver.find_element_by_xpath("//*[@id='car_privatebusiness_label']").click()
+			time.sleep(1)
+			driver.find_element_by_xpath("//*[@id='smiForm']/div[3]/div/div/div/div/li/button").click()
+			#wait.until(EC.element_located_to_be_selected((By.ID,'smiForm')))
+		
+			#----------------------------------------------------------------------------------------------------
+			driver.find_element_by_xpath("//*[@id='smiForm']/div[2]/div/ul/li[1]/div[1]/div/label[2]").click()
+			driver.find_element_by_xpath("//*[@id='smiForm']/div[2]/div/ul/li[3]/div[1]/div/label[2]").click()
+			driver.find_element_by_xpath("//*[@id='smiForm']/div[2]/div/ul/li[5]/div[1]/div/label[2]").click()
+			driver.find_element_by_xpath("//*[@id='schaeden']/div[1]/div/label[2]").click()
+			driver.find_element_by_xpath("//*[@id='weiter']").click()
+			#-----------------------------------------------------------------------------------------------------
+			if leasing == 'no':
+				time.sleep(2)
+				driver.find_element_by_xpath("//*[@id='haftpflicht_content']/table/tbody/tr[2]/td[2]/div/div/div/div/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div/div/div/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='insassenunfall_acc']/li/div/div/div/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='gfverzicht_mod']/div/ul/li/div/div/div/div/label[1]").click()
+				time.sleep(5)
+				firstValue = driver.find_element_by_xpath("//*[@id='bruttopraemie']").text
 			
 			
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[1]/div/div/label[2]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_inner']/a").click()
-			time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[1]/div/div/label[2]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_inner']/a").click()
+				time.sleep(5)
 			
-			if deductibleTeil == 300:
-				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[3]").click()
-			elif deductibleTeil == 0:
-				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[17]/td[2]/div/div/div/div[1]/label[1]").click()
-			time.sleep(5)
-			secondValue = driver.find_element_by_xpath("//*[@id='bruttopraemie']").text
+				if deductibleTeil == 300:
+					driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[3]").click()
+				elif deductibleTeil == 0:
+					driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[17]/td[2]/div/div/div/div[1]/label[1]").click()
+				time.sleep(5)
+				secondValue = driver.find_element_by_xpath("//*[@id='bruttopraemie']").text
 			
 		
 		
 	
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[1]/div/div/label[3]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[2]/td[2]/div/div/div/div/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[4]/td[2]/div/div/div/div[1]/label[1]").click()
-			
-			if deductibleTeil == 300:
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[1]/div/div/label[3]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[2]/td[2]/div/div/div/div/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[4]/td[2]/div/div/div/div[1]/label[1]").click()
+				driver.sleep(3)
+				if deductibleTeil == 300:
 
-				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[3]").click()
-			elif deductibleTeil == 0:
-				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[16]/td[2]/div/div/div/div[1]/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[17]/td[2]/div/div/div/div[1]/label[1]").click()
-			time.sleep(5)
-			thirdValue = driver.find_element_by_xpath("//*[@id='bruttopraemie']").text
+					driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[3]").click()
+				elif deductibleTeil == 0:
+					driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[16]/td[2]/div/div/div/div[1]/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[17]/td[2]/div/div/div/div[1]/label[1]").click()
+				time.sleep(5)
+				thirdValue = driver.find_element_by_xpath("//*[@id='bruttopraemie']").text
 			
-			ws['W'+index] = firstValue
-			ws['X'+index] = secondValue
-			ws['Y'+index] = thirdValue
-		else:
+				ws['W'+index] = firstValue
+				ws['X'+index] = secondValue
+				ws['Y'+index] = thirdValue
+			else:
 			
-			driver.find_element_by_xpath("//*[@id='haftpflicht_content']/table/tbody/tr[2]/td[2]/div/div/div/div/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_inner']/a").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[1]/div/div/label[3]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[2]/td[2]/div/div/div/div/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[4]/td[2]/div/div/div/div[1]/label[1]").click()
-			time.sleep(5)
-			if deductibleTeil == 300:
-				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[3]").click()
+				driver.find_element_by_xpath("//*[@id='haftpflicht_content']/table/tbody/tr[2]/td[2]/div/div/div/div/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_inner']/a").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[1]/div/div/label[3]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[2]/td[2]/div/div/div/div/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[4]/td[2]/div/div/div/div[1]/label[1]").click()
+				time.sleep(5)
+				if deductibleTeil == 300:
+					driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[3]").click()
 				
-			elif deductibleTeil == 0:
-				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[16]/td[2]/div/div/div/div[1]/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='insassenunfall_acc']/li/div/div/div/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[17]/td[2]/div/div/div/div[1]/label[1]").click()
-			time.sleep(5)
-			driver.find_element_by_xpath("//*[@id='assistance_acc']/li/div[1]/div/div/label[1]").click()
-			driver.find_element_by_xpath("//*[@id='gfverzicht_mod']/div/ul/li/div/div/div/div/label[1]").click()
-			thirdValue = driver.find_element_by_xpath("//*[@id='bruttopraemie']").text
+				elif deductibleTeil == 0:
+					driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[5]/td[2]/div/div/div/div[1]/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[16]/td[2]/div/div/div/div[1]/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='insassenunfall_acc']/li/div/div/div/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='kasko_acc']/li/div[2]/table/tbody/tr[17]/td[2]/div/div/div/div[1]/label[1]").click()
+				time.sleep(5)
+				driver.find_element_by_xpath("//*[@id='assistance_acc']/li/div[1]/div/div/label[1]").click()
+				driver.find_element_by_xpath("//*[@id='gfverzicht_mod']/div/ul/li/div/div/div/div/label[1]").click()
+				thirdValue = driver.find_element_by_xpath("//*[@id='bruttopraemie']").text
 			
-			ws['Y'+index] = thirdValue
+				ws['Y'+index] = thirdValue
 		
 		
-	except:
-		#print "An error occured at: "+str(sdId)
-		
-		output = open('log.txt','a')
-		output.write("Failed record with ID: "+str(sdId)+"\n")
-		output.close()
-		wb.save("smile main sample.xlsm")
-		pass
-	finally:
-		
-		driver.close()
-		driver.quit()
-wb.save("smile main sample.xlsm")
-successBar.finish()
+		except:
+			#print "An error occured at: "+str(sdId)
+			pbar.close()
+			output = open('log.txt','a')
+			output.write("Failed record with ID: "+str(sdId)+"\n")
+			output.close()
+			wb.save("smile main sample.xlsm")
+			pass
+		finally:
+			pbar.update(1)
+			driver.close()
+			driver.quit()
+except:
+	print("External Error")
+finally:
+	wb.save("smile main sample.xlsm")
+	pbar.close()
+	
 
 
 
